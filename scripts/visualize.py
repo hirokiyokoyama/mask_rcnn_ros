76e091987595215cfd12f169184051ae55002d06
Hiroki Yokoyama <hiroki.yokoyama@okadanet.org>
2019-02-01 18:19:39 +0900

    add visualize.py

---
+++ scripts/visualize.py
@@ -0,0 +1,29 @@
+import rospy
+from sensor_msgs.msg import Image
+from mask_rcnn.client import Client
+import numpy as np
+import cv2
+from cv_bridge import CvBridge
+
+rospy.init_node('visualize_mask_rcnn')
+mask_rcnn = Client()
+bridge = CvBridge()
+
+def cb(imgmsg):
+    #vis = mask_rcnn.visualize_imgmsg(imgmsg)
+    image = bridge.imgmsg_to_cv2(imgmsg, 'bgr8')
+    masks = mask_rcnn.process_image(image, 'bgr8')
+    total = np.zeros(image.shape[:2]+(1,), dtype=np.float32)
+    for mask in masks:
+        if mask.name != 'person':
+            continue
+        print '%s (%f%%)' % (mask.name, mask.probability*100)
+        total += np.expand_dims(mask.mask, -1) * mask.probability
+    total[total>1.] = 1.
+    #total[total<1.5] = 0.
+    cv2.imshow('MaskRCNN', np.uint8(image*total))
+    cv2.imshow('original', image)
+    cv2.waitKey(1)
+
+rospy.Subscriber('/hsrb/head_rgbd_sensor/rgb/image_rect_color', Image, cb)
+rospy.spin()
